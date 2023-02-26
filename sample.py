6a8ae79b2751160282fa55463174a2b970120600
jonathancruzz <jonathan.cruz@pak2go.com>
2023-02-25 21:30:31 -0600

    Plot add

--- sample.py
+++ sample.py
@@ -5,23 +5,15 @@ import numpy as np
 from scipy.io import wavfile as wf
 
 
-data, samplerate = sf.read('talk.wav')
-fig = plt.figure()
-plt.plot(data)
-plt.title("Muestra de prueba")
-fig.tight_layout()
-plt.show()  
-
-data, samplerate = sf.read('success.wav')
-fig = plt.figure()
-plt.plot(data)
-plt.title("Funcional")
-fig.tight_layout()
-plt.show()  
+ 
 
 f = sf.SoundFile('talk.wav')
 print('Talk samples = {}'.format(f.frames))
 print('sample rate = {}'.format(f.samplerate))
+print('channels = {}'.format(f.channels))
+print('format = {}'.format(f.format))
+print('subtype = {}'.format(f.subtype))
+print('extra_info = {}'.format(f.extra_info))
 print('seconds = {}'.format(f.frames / f.samplerate))
 
 
@@ -29,4 +21,8 @@ print('seconds = {}'.format(f.frames / f.samplerate))
 f = sf.SoundFile('success.wav')
 print('Success samples = {}'.format(f.frames))
 print('sample rate = {}'.format(f.samplerate))
+print('channels = {}'.format(f.channels))
+print('format = {}'.format(f.format))
+print('subtype = {}'.format(f.subtype))
+print('extra_info = {}'.format(f.extra_info))
 print('seconds = {}'.format(f.frames / f.samplerate))
