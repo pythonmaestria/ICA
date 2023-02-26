e0ec72c3cd4e23e8a46142fe8e83e555a9d1cbaf
jonathancruzz <jonathan.cruz@pak2go.com>
2023-02-25 21:18:05 -0600

    Plot add

--- sample.py
+++ sample.py
@@ -5,6 +5,19 @@ import numpy as np
 from scipy.io import wavfile as wf
 
 
+data, samplerate = sf.read('talk.wav')
+fig = plt.figure()
+plt.plot(data)
+plt.title("Música")
+fig.tight_layout()
+plt.show()  
+
+data, samplerate = sf.read('success.wav')
+fig = plt.figure()
+plt.plot(data)
+plt.title("Música")
+fig.tight_layout()
+plt.show()  
 
 f = sf.SoundFile('talk.wav')
 print('Talk samples = {}'.format(f.frames))
